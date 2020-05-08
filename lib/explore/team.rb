690b18b3c8ae76addb1a1c791f4ed3afaa4ed73c
antdp425 <antdp425@aol.com>
2020-05-08 03:04:24 -0400

    removes key

--- lib/explore/team.rb
+++ lib/explore/team.rb
@@ -10,8 +10,16 @@ class Team
       save
    end
 
+   def win_percent
+      (self.wins.to_f / self.matches_played.to_f * 100).to_i
+   end
+
    def self.all
       @@all.sort_by{|t| t.name}
+   end
+
+   def self.sort_by_wins
+      @@all.sort{|a,b| b.wins <=> a.wins}
    end
 
    def save
